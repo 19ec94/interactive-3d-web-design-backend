333dd40a2716e5d93986ad1198b8a0b8c7356e4f
19ec94 <bert.edil@gmail.com>
2024-01-05 13:47:43 +0100

    Add source files for models, controllers, routes

---
+++ app/models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+// Create a schema for a database entry
+const userDataSchema = new mongoose.Schema({
+  userName: { 
+    type: String,
+    required: [true, 'Provide a username'],
+  },
+  userEmail: {
+    type: String,
+    required: [true, 'Provide a valid email address'],
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: (value) => isEmail(value),
+      message: 'Invalid Email address',
+    },
+  },
+  userPassword: {
+    type: String,
+    required: [true, 'Provide a non-empty password'],
+  }
+});
+
+userDataSchema.pre('save', async function(next) {
+  try {
+    // Return an error if username is empty
+    if (this.userName.trim().length === 0) {
+      throw new Error('Username cannot be empty');
+    }
+    // If password is not empty, hash the password,
+    // else return an error message
+    if (this.userPassword.trim().length > 0) {
+      const hashedPassword = await bcrypt.hash(this.userPassword, 10);
+      this.userPassword = hashedPassword;
+    } else {
+      throw new Error('Password cannot be empty');
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Create a database named userDB 
+const userDB =  mongoose.model("userDB", userDataSchema);
+module.exports  = userDB;
+// NOTE: In mongodb database, you find a collection `userdbs` in admin database
