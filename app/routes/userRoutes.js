3bdf02365e3823f7efafad18f60bca9c5363c76d
19ec94 <bert.edil@gmail.com>
2024-01-10 16:13:07 +0100

    Refer the commit message in 4ba80e35f400bcabad661af151805f0d4d6f0ab3

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const userValidator = require('../validators/userValidator');
 
-router.post('/signup', userController.createUser);
-router.post('/login', userController.loginUser);
-router.delete('/:username', userController.deleteUserById);
+router.post('/signup',
+  userValidator.signupDataValidator,
+  userController.createUser
+);
+router.post('/login', 
+  userValidator.loginDataValidator,
+  userController.loginUser
+);
+router.delete('/:username', 
+  userController.deleteUserById
+);
 
 module.exports = router;
