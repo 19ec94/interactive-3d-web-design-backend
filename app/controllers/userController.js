d641d3bc3502500551bc82b4205a537fcad1279a
19ec94 <bert.edil@gmail.com>
2024-01-10 16:16:51 +0100

    Add support for token creation upon signup/login
    This commit will
     - add jsonwebtoken library
     - create a token after sucessful signup or login

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt'); 
 const userDB = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
   try {
@@ -9,18 +10,16 @@ exports.createUser = async (req, res) => {
       userEmail: req.body.user_email,
       userPassword: req.body.user_password, 
     });
-    // Check if the username already exists  
     const existingUser = await userDB.findOne({userName: user.userName});
-    // findOne returns `null` if the username is not present in database. 
-    // If it is `null`, create a new user,
-    // else return the error message "username is already taken". 
-    if (existingUser === null){
-      // Parse and verify if the user provided "all" the required data.
-      // Refer the schema for verification rule in models/signup.js.
-      // Save the entry in the database.
+    if (!existingUser){
       const newUser = await user.save();
+      const secretKey = process.env.SECRETKEY || 'default_key';
+      const token = jwt.sign(
+        {userId: newUser._id}, 
+        secretKey, 
+        {expiresIn:'1h'});
       // Send response
-      res.status(201).json(newUser);
+      res.status(201).json({token});
       console.log(newUser);// debugging
     }
     else {
@@ -30,7 +29,7 @@ exports.createUser = async (req, res) => {
       });
     }
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -44,7 +43,7 @@ exports.loginUser = async (req, res)=> {
     // Check if the username exists in the database
     const existingUser = await userDB.findOne({userName: login.userName});
     // Return error message if it does not exist
-    if (existingUser == null) {
+    if (!existingUser) {
       res.status(400).json({message: 'Username does not exist'});
     } else {
       // Check if the password matches
@@ -52,16 +51,19 @@ exports.loginUser = async (req, res)=> {
         login.userPassword, existingUser.userPassword);
       if (!isPasswordMatch) {
         res.status(200).json({message: 'Incorrect password'});
-        //TODO: Dashboard
       } else {
-        res.send("login complete");
+        const secretKey = process.env.SECRETKEY || 'default_key';
+        const token = jwt.sign(
+          {userId: existingUser._id}, 
+          secretKey, 
+          {expiresIn:'1h'});
+        // Send response
+        res.status(201).json({token});
         console.log("User account verified");
       }
     }
   } catch(err) {
-    res.status(400).json({ 
-      message: "Provide a valid username and password!" 
-    });
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -78,6 +80,5 @@ exports.deleteUserById = async (req, res) => {
   } catch (err) {
     console.error('Error deleting user:', err.message);
     res.status(500).json({message: 'Internal Server Error'});
-    console.log({message: 'Deleted account successfully'})
   }
 };
