60d349ebeb6f52199b2da6239f34e15a6b4330f4
19ec94 <bert.edil@gmail.com>
2024-01-12 10:34:56 +0100

    Remove the old controller logic for addUserToDatabase

--- controllers/user.controller.index.js
+++ controllers/user.controller.index.js
@@ -2,13 +2,10 @@
  * Import module dependencies.
  */
 
-const addUserToDatabase = require("./addUserToDatabase.controller");
 const sendSessionToken = require("./sendSessionToken.controller");
 const deleteUserById = require("./deleteAccount.controller");
 
-
 module.exports = {
-  addUserToDatabase,
   sendSessionToken,
   deleteUserById,
 }
